d3b5de8137991b9af15a4ed9d3bf06e63b969b48
Decadz <christianfraymond@gmail.com>
2019-01-07 09:52:38 +1300

    Added new graphs on new data.

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -17,10 +17,10 @@ classic_ccn_path = "../output/benchmark_classic/benchmark-classic-ccn-"
 classic_ccun_path = "../output/benchmark_classic/benchmark-classic-ccun-"
 classic_ld50_path = "../output/benchmark_classic/benchmark-classic-ld50-"
 
-rademacher_cd_path = "../output/experimental_rademacher_complexity/experimental-rademacher-complexity-cd-"
-rademacher_ccn_path = "../output/experimental_rademacher_complexity/experimental-rademacher-complexity-ccn-"
-rademacher_ccun_path = "../output/experimental_rademacher_complexity/experimental-rademacher-complexity-ccun-"
-rademacher_ld50_path = "../output/experimental_rademacher_complexity/experimental-rademacher-complexity-ld50-"
+rademacher_cd_path = "../output/experimental_rademacher_complexity/experimental-rademacher-complexity-cd-v2-"
+rademacher_ccn_path = "../output/experimental_rademacher_complexity/experimental-rademacher-complexity-ccn-v2-"
+rademacher_ccun_path = "../output/experimental_rademacher_complexity/experimental-rademacher-complexity-ccun-v2-"
+rademacher_ld50_path = "../output/experimental_rademacher_complexity/experimental-rademacher-complexity-ld50-v2-"
 
 
 def main():
@@ -38,16 +38,16 @@ def main():
     rademacher_ld50 = load_data(rademacher_ld50_path)
 
     # Analysing the Genetic Programming classic output data.
-    bc1 = analyse(classic_cd, "benchmark-classic", "cd")
-    bc2 = analyse(classic_ccn, "benchmark-classic", "ccn")
-    bc3 = analyse(classic_ccun, "benchmark-classic", "ccun")
-    bc4 = analyse(classic_ld50, "benchmark-classic", "ld50")
+    bc1 = analyse_classic(classic_cd, "benchmark-classic", "cd")
+    bc2 = analyse_classic(classic_ccn, "benchmark-classic", "ccn")
+    bc3 = analyse_classic(classic_ccun, "benchmark-classic", "ccun")
+    bc4 = analyse_classic(classic_ld50, "benchmark-classic", "ld50")
 
     # Analysing the Genetic Programming with Rademacher Complexity output data.
-    er1 = analyse(rademacher_cd, "experimental-rademacher", "cd")
-    er2 = analyse(rademacher_ccn, "experimental-rademacher", "ccn")
-    er3 = analyse(rademacher_ccun, "experimental-rademacher", "ccun")
-    er4 = analyse(rademacher_ld50, "experimental-rademacher", "ld50")
+    er1 = analyse_rademacher(rademacher_cd, "experimental-rademacher", "cd")
+    er2 = analyse_rademacher(rademacher_ccn, "experimental-rademacher", "ccn")
+    er3 = analyse_rademacher(rademacher_ccun, "experimental-rademacher", "ccun")
+    er4 = analyse_rademacher(rademacher_ld50, "experimental-rademacher", "ld50")
 
     visualise(bc1, "benchmark-classic", er1, "experimental-rademacher", "CD")
     visualise(bc2, "benchmark-classic", er2, "experimental-rademacher", "CCN")
@@ -79,7 +79,7 @@ def load_data(output_path):
     return data
 
 
-def analyse(data, algorithm_name, data_name):
+def analyse_classic(data, algorithm_name, data_name):
 
     """
     Merges the data (taking the averages across all runs) and than extracts the key information.
@@ -103,6 +103,54 @@ def analyse(data, algorithm_name, data_name):
         gen_num.append(data[index][["generation_num"]])
         gen_time.append(data[index][["generation_time"]])
         training_fitness.append(data[index][["training_fitness_min"]])
+        testing_fitness.append(data[index][["test_mse"]])
+        size_med.append(data[index][["size_med"]])
+
+    result = list()
+
+    gen_num = sum(gen_num)/num_samples
+    gen_time = sum(gen_time)/num_samples
+    training_fitness = sum(training_fitness)/num_samples
+    testing_fitness = sum(testing_fitness)/num_samples
+    size_med = sum(size_med)/num_samples
+
+    for i in range(len(gen_num)):
+
+        result.append([gen_num.iloc[i].values[0], gen_time.iloc[i].values[0], training_fitness.iloc[i].values[0],
+                testing_fitness.iloc[i].values[0], size_med.iloc[i].values[0]])
+
+
+    df = pd.DataFrame(result)
+    df.columns = ["Generation Number", "Generation Time", "Training Error", "Testing Error", "Average Size"]
+    df.to_csv(algorithm_name + "-" + data_name + ".csv", index=False)
+
+    return df
+
+
+def analyse_rademacher(data, algorithm_name, data_name):
+
+    """
+    Merges the data (taking the averages across all runs) and than extracts the key information.
+    The pandas dataframe in exported to a csv file which appears in the analysis folder.
+
+    :param data:
+    :param algorithm_name:
+    :param data_name:
+    :return:
+    """
+
+    gen_num = list()
+    gen_time = list()
+    training_fitness = list()
+    testing_fitness = list()
+    size_med = list()
+
+    # Extracting the important columns.
+    for index in range(num_samples):
+
+        gen_num.append(data[index][["generation_num"]])
+        gen_time.append(data[index][["generation_time"]])
+        training_fitness.append(data[index][["errors_min"]])
         testing_fitness.append(data[index][["test_mse"]])
         size_med.append(data[index][["size_med"]])
 
@@ -194,6 +242,7 @@ def visualise_error(df1, df1_name, df2, df2_name, dataset_name):
 
     # Saving the diagram to the analysis folder (CWD).
     plt.savefig("classic-vs-rademacher-error-" + dataset_name + ".png")
+    plt.show()
     plt.close()
 
 
